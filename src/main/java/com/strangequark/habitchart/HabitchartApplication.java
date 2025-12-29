d7289fd08205cb31b7bab33eeadbe98e839caaa7
StrangeQuark <zacharyzirkle@hotmail.com>
2025-12-28 22:58:40 -0600

    Initial commit

---
+++ src/main/java/com/strangequark/habitchart/HabitchartApplication.java
@@ -0,0 +1,13 @@
+package com.strangequark.habitchart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HabitchartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HabitchartApplication.class, args);
+	}
+
+}
